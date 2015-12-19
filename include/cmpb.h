4600d54abc38d28e181075367a07bfb07e96b633
Miles Lubin <miles.lubin@gmail.com>
2015-12-19 17:34:56 -0500

    not working yet

---
+++ include/cmpb.h
@@ -0,0 +1,15 @@
+
+
+
+#define MPBFREECONE 0
+#define MPBZEROCONE 1
+#define MPBNONNEGCONE 2
+#define MPBNONPOSCONE 3
+#define MPBSOC 4
+#define MPBSOCROTATED 5
+#define MPBSDPCONE 6
+#define MPBEXPPRIMAL 7
+#define MPBEXPDUAL 8
+
+int mpb_initialize(void);
+
